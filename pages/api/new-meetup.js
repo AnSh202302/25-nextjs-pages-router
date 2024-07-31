96a1f50c5fa8390a987ca1eb69b2d17d9c37703b
AnSh202302 <125475748+AnSh202302@users.noreply.github.com>
2024-07-31 18:23:43 +0200

    Sending Http Requests To Our API Routes

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -2,14 +2,17 @@
 import { MongoClient } from "mongodb";
 
 async function handler(req, res) {
+  console.log("req", req);
   if (req.method === "POST") {
     const data = req.body;
 
     const client = await MongoClient.connect(
-      "mongodb+srv://Antonina:aje79TNsMXsiJpRc@cluster0.lideuo9.mongodb.net/meetups?retryWrites=true&w=majority&appName=Cluster0"
+      "mongodb+srv://Antonina:pGWZI2pmE6oxYPoz@cluster0.lideuo9.mongodb.net/meetups?retryWrites=true&w=majority&appName=Cluster0"
     );
+
     const db = client.db();
     const meetupsCollection = db.collection("meetups");
+
     const result = await meetupsCollection.insertOne(data);
     console.log(result);
     client.close();
